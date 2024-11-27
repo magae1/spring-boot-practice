3e0eee7858253c27ea0e2dc48cb54d2a8f911bcd
magae1 <jjilil8351@gmail.com>
2024-11-27 10:01:15 +0900

    🎉 부트 프로젝트 시작

---
+++ src/test/java/org/conan/bootpractice/BootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.conan.bootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
